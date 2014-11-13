48d04132f68d103078fe8eff2dd459c6b3310267
Hylke Hendriksen <HyHend@HH-rMBP.local>
2014-11-13 07:42:48 +0100

    Added popup

--- invite-overview.js
+++ invite-overview.js
@@ -60,6 +60,6 @@ for (var i=0; i<paymentInvites.length; i++) {
   	paymentInvite = paymentInvites[i];
   	block = document.createElement('div');
   	block.setAttribute("class","pi-block-wrapper");
-  	block.innerHTML = '<div class="pi-block pi-block-status-'+paymentInvite.status.toLowerCase()+'" onclick="window.location.href = \'#payment\'"><h3 class="pi-block-name">'+paymentInvite.firstName+' '+paymentInvite.lastName+'</h3><h5 class="pi-block-date">'+paymentInvite.date+'</h5><h3 class="pi-block-amount">&euro; '+paymentInvite.amount+'</h3><h5 class="pi-block-status">'+paymentInvite.status+'</h5></div>';
+  	block.innerHTML = '<div class="pi-block pi-block-status-'+paymentInvite.status.toLowerCase()+'" onclick="window.location.href = \'#payment\'"><h3 class="pi-block-name">'+paymentInvite.firstName+' '+paymentInvite.lastName+'</h3><a  href="#popupDialog" data-rel="popup" data-position-to="window" data-transition="pop" class="pi-block-cancel" onclick="">x</a><h5 class="pi-block-date">'+paymentInvite.date+'</h5><h3 class="pi-block-amount">&euro; '+paymentInvite.amount+'</h3><h5 class="pi-block-status">'+paymentInvite.status+'</h5></div>';
   	piBlocks.appendChild(block);
 }
